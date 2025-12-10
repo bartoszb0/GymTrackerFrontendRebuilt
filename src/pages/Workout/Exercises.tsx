ab323b91f2307327fb9fdb8149b844c7e8a0d84f
bartoszb0 <bartoszb2020@gmail.com>
2025-12-10 20:32:03 +0100

    started working on displaying exercises

--- src/pages/Workout/Exercises.tsx
+++ src/pages/Workout/Exercises.tsx
@@ -1,28 +1,69 @@
-import { ScrollArea, SimpleGrid } from "@mantine/core";
+import {
+  Button,
+  Card,
+  ScrollArea,
+  SimpleGrid,
+  Stack,
+  Text,
+} from "@mantine/core";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import NotFoundArray from "../../components/NotFoundArray";
+import type { Exercise } from "../../types/types";
 import api from "../../utils/api";
 
 type ExercisesProps = {
   id: number;
+  isDeletingExercise: boolean;
+  setIsDeletingExercise: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export default function Exercises({ id }: ExercisesProps) {
-  const { data: exercises } = useSuspenseQuery<[]>({
+export default function Exercises({
+  id,
+  isDeletingExercise,
+  setIsDeletingExercise,
+}: ExercisesProps) {
+  const { data: exercises } = useSuspenseQuery<Exercise[]>({
     queryKey: ["workout", id],
     queryFn: () => api.get(`workouts/${id}/exercises/`).then((res) => res.data),
   });
 
-  console.log(exercises);
+  const exercisesElement = exercises.map((exercise) => {
+    return (
+      <Card
+        mb="sm"
+        mt="sm"
+        key={exercise.id}
+        onClick={() => console.log(exercise.id)}
+      >
+        <Text size="35px" m="3px">
+          {exercise.name} - {exercise.sets}x{exercise.reps} - {exercise.weight}
+        </Text>
+        {isDeletingExercise && <Button bg="red.8">Delete</Button>}
+      </Card>
+    );
+  });
 
   return (
     <>
       {exercises.length <= 0 ? (
         <NotFoundArray arrayName="exercise" />
       ) : (
-        <SimpleGrid cols={1}>
-          <ScrollArea mah="60vh">X</ScrollArea>
-        </SimpleGrid>
+        <Stack>
+          <SimpleGrid cols={1}>
+            <ScrollArea mah="60vh">{exercisesElement}</ScrollArea>
+          </SimpleGrid>
+          {!isDeletingExercise && (
+            <>
+              <Button size="xl">Start workout</Button>
+              <Button size="xl" onClick={() => setIsDeletingExercise(true)}>
+                Delete exercise
+              </Button>
+            </>
+          )}
+          {isDeletingExercise && (
+            <Button onClick={() => setIsDeletingExercise(false)}>Done</Button>
+          )}
+        </Stack>
       )}
     </>
   );
