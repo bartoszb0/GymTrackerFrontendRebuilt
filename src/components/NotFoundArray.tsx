17b87d568aeb70ac3e686155fb430e504460aad7
bartoszb0 <bartoszb2020@gmail.com>
2025-12-10 17:49:54 +0100

    changed NotFoundArray component styling

--- src/components/NotFoundArray.tsx
+++ src/components/NotFoundArray.tsx
@@ -6,8 +6,8 @@ type NotFoundArrayProps = {
 
 export default function NotFoundArray({ arrayName }: NotFoundArrayProps) {
   return (
-    <Flex align="center" justify="center">
-      <Text fs="italic" size="30px" mt="xl">
+    <Flex align="center" justify="center" bg="dark.8">
+      <Text fs="italic" size="30px" m="xl">
         No {arrayName} found
       </Text>
     </Flex>
